9b69be455eb6fc264f29ce7ca5f761a2f8b70f32
Val <valkurash@gmail.com>
2014-09-19 20:37:07 +0400

    Добавил библиотеки под контроль версий

--- GiftKnacksProject.Site/GiftKnacksProject.Site/obj/Debug/server.js
+++ GiftKnacksProject.Site/GiftKnacksProject.Site/obj/Debug/server.js
@@ -1,6 +1,17 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var connect = require('connect');
+var express = require('express')
+  , exphbs  = require('express3-handlebars')
+  , app = express()
+  , port = (process.env.PORT || 80);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ', port);
+});
