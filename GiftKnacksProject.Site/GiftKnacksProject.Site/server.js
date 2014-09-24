9620e7d1a1b817100b1393c1876d1a1051eec99e
Val <valkurash@gmail.com>
2014-09-24 23:08:19 +0400

    Добавлен sass и bootstrap

--- GiftKnacksProject.Site/GiftKnacksProject.Site/server.js
+++ GiftKnacksProject.Site/GiftKnacksProject.Site/server.js
@@ -3,15 +3,32 @@ var express = require('express')
   , exphbs  = require('express3-handlebars')
   , app = express()
   , port = (process.env.PORT || 80);
+var sass = require('node-sass');
+
+//mapping sass dir to css dir
+app.use(sass.middleware({
+    src: __dirname + '/Sass',
+    dest: __dirname + '/Styles',
+    debug: true,
+    outputStyle: 'compressed'
+}));
+
+app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+    var data = {
+        title: "Node + Handlebars",
+        body: "Hello World!"
+    }
+    res.render('home', data);
 });
 
+
 app.listen(port, function () {
+
   console.log('Listening on port ', port);
 });
