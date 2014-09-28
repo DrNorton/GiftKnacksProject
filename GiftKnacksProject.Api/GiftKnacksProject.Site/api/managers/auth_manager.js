f410f2c5a44e830e79e2d2f73a7362bd74a9ffa4
Dimon <kozak_andrews@mail.ru>
2014-09-28 17:53:08 +0400

    Переименовал проект, выпилил saas. Теперь тольк о api

---
+++ GiftKnacksProject.Api/GiftKnacksProject.Site/api/managers/auth_manager.js
@@ -0,0 +1,19 @@
+﻿var SqlManager = require("./sql_manager").SqlManager;
+//var PasswordHelper = require("./passwordHelper").PasswordHelper;
+
+
+var AuthManager = function () {
+    this.sqlManager = new SqlManager();
+
+};
+
+
+AuthManager.prototype.logon = function (params, onSuccess, onError) {
+    var self = this;
+
+};
+
+
+
+
+exports.AuthManager = AuthManager;
---
+++ GiftKnacksProject.Api/GiftKnacksProject.Site/server.js
@@ -0,0 +1,37 @@
+﻿var config = require("nconf");
+var express = require('express');
+var passport = require('passport');
+var app = express();
+
+config.argv()
+    .env()
+    .file({ file: 'config.json' });
+//boot
+require('./boot/index')(app);
+//routers
+app.use('/api', require('./route_configs/api_routes_config').router);
+
+
+
+
+
+
+app.post('/auth', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth',
+    failureFlash: true
+})
+  );
+
+app.get('/auth', function (req, res) {
+    var data = {
+        title: "Node + Handlebars",
+        body: "Hello World!"
+    };
+    res.render('auth', data);
+});
+
+app.listen(app.get('port'), function () {
+
+    console.log('Listening on port ', app.get('port'));
+});
