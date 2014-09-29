cf8deaf29772f7d29a772be5360d3eb672e8340d
Val <valkurash@gmail.com>
2014-09-30 01:50:24 +0400

    установка angularjs

---
+++ GiftKnacksProject.Site/GiftKnacksProject.Site/js/project.js
@@ -0,0 +1,19 @@
+﻿var project = angular.module('giftKnacksProject', ['ngRoute']);
+project.controller(
+'AppCtrl',
+[
+'$scope',
+'$log',
+function ($scope, $log) {
+  $scope.handleMyButton = function () {
+    $log.log('Angular Button clicked');
+  }
+}
+]
+);
+project.config(specifyRoutes);
+function specifyRoutes($routeProvider) {
+  $routeProvider
+  .when('/', { templateUrl: 'partials/home.html' })
+  .when('/pagea', { templateUrl: 'partials/pagea.html' });
+}
