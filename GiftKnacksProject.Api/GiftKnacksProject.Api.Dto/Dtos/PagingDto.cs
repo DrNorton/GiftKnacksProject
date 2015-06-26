bafede922fbd5bcac411a73c11d3d07a32ed7308
Andrew Home <dimaivanov1@mail.ru>
2015-06-26 15:05:08 +0300

    -

---
+++ GiftKnacksProject.Api/GiftKnacksProject.Api.Dto/Dtos/PagingDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftKnacksProject.Api.Dto.Dtos
+{
+    public class PagingDto
+    {
+        public int Offset { get; set; }
+        public int Length { get; set; }
+    }
+}
