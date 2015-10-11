f8e75bf282f9389c2c36fa0948140cd1f9bfc872
Andrew Kozak <dimaivanov1@mail.ru>
2015-10-11 04:09:14 +0300

    -

--- GiftKnacksProject.Api/GiftKnacksProject.Api.Controllers/Hubs/OnlineHub.cs
+++ GiftKnacksProject.Api/GiftKnacksProject.Api.Controllers/Hubs/OnlineHub.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
 
 namespace GiftKnacksProject.Api.Controllers.Hubs
 {
-    class OnlineHub
+    public class OnlineHub:Hub
     {
+        private int _userCount;
+
+        public override Task OnConnected()
+        {
+            _userCount++;
+            var context = GlobalHost.ConnectionManager.GetHubContext<OnlineHub>();
+            context.Clients.All.online(_userCount);
+        }
+
+        public override Task OnReconnected()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<OnlineHub>();
+            context.Clients.All.online(_userCount);
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _userCount--;
+            var context = GlobalHost.ConnectionManager.GetHubContext<OnlineHub>();
+            context.Clients.All.online(_userCount);
+        }
     }
 }
