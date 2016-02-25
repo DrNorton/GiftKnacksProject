6de0a831ebf98a88af56134942c908b2f2b86475
Val Kurashova <valkurash@gmail.com>
2016-02-25 14:29:28 +0300

    публикация api

--- GiftKnacksProject.Api/GiftKnackProject.NotificationTypes/AddCommentQueueNotification.cs
+++ GiftKnacksProject.Api/GiftKnackProject.NotificationTypes/AddCommentQueueNotification.cs
@@ -2,7 +2,13 @@
 {
     public  class AddCommentQueueNotification: UserGeneratedQueueNotification
     {
-        public override string Type => "addcomment";
+        public override string Type
+        {
+            get
+            {
+                return "addcomment";
+            }
+        }
         public long TargetId { get; set; }
         public  string TargetType { get; set; }
         public long CommentId { get; set; }
